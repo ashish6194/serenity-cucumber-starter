6b255e5536ac91421cc7206ab828e0cc54b15ed1
Ashish Kuamr Sharma <ashish6194@users.noreply.github.com>
2022-05-12 21:28:25 +0530

    Update CucumberTestSuite.java

--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json"},
+        plugin = {"pretty", "json:target/Cucumber.json"},
         features = "src/test/resources/features"
 )
 public class CucumberTestSuite {}
